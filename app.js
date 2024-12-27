5b33125d7d6ecb99dbfe389f59ebcadb9d18ea23
shila <shilapattewar16@gmail.com>
2024-12-27 01:35:37 -0600

    check the accuracy and attack

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 // create object literals
-const ussAssembly = {
-    hull: 20,
-    firepower: 5,
-    accuracy: 0.7
-}
+    const ussAssembly = {
+        hull: 20,
+        firepower: 5,
+        accuracy: 0.7
+    }
 
 // The alien ships should each have the following ranged properties determined randomly
   
-function generateAlienShip(){
-    return {
-        hull: Math.floor(Math.random() * (6 - 3 + 1)) + 3, 
-        firepower: Math.floor(Math.random() * (4 - 2 + 1)) + 2, 
-        accuracy: Math.random() * (0.8 - 0.6) + 0.6
-    };
-    
+    function generateAlienShip(){
+        return {
+            hull: Math.floor(Math.random() * (6 - 3 + 1)) + 3, 
+            firepower: Math.floor(Math.random() * (4 - 2 + 1)) + 2, 
+            accuracy: Math.random() * (0.8 - 0.6) + 0.6
+        };
+        
     }
-    console.log(generateAlienShip);
+      //  console.log(generateAlienShip()); // shows all ramdom alianship properties ie hull firepower and accuracy
 
-    const alienShips = [];
-    for (let i = 0; i <= 5; i++) {
-        alienShips.push(generateAlienShip());
+        const alienShips = [];
+        for (let i = 0; i <= 5; i++) {     // generate 6 alienships
+            alienShips.push(generateAlienShip());
+        }
+        //console.log(alienShips);  // shows all six alianships
+        console.log(alienShips[0]);  // shows only 1st alianship
+
+//You could be battling six alien ships each with unique values.
+//Example use of accuracy to determine a hit:
+//You attack the first alien ship i.e. alienShips[0]
+
+    if (Math.random() < alienShips[0].accuracy) {
+        console.log('You have been hit!');
     }
-  
-    console.log(alienShips); 
+    else{
+        console.log("you survive");
+    }

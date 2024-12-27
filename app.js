b8bb6f173585bfc679639462f6b148d36f4d2cc3
shila <shilapattewar16@gmail.com>
2024-12-27 00:50:14 -0600

    generate ramdom hull , firepower and accuracy

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ const ussAssembly = {
 
 // The alien ships should each have the following ranged properties determined randomly
 
-function generateAlienShip() {
-    return {
-      hull: Math.floor(Math.random() * (6 - 3 + 1)) + 3 
-    };
-  }
+// function generateAlienShip() {
+//     return {
+//       hull: Math.floor(Math.random() * (6 - 3 + 1)) + 3 
+//     };
+//   }
   
-const alienShips = [];
-for (let i = 0; i <= 5; i++) {
-    alienShips.push(generateAlienShip());
-}
+// const alienShips = [];
+// for (let i = 0; i <= 5; i++) {
+//     alienShips.push(generateAlienShip());
+// }
   
-console.log(alienShips); 
+// console.log(alienShips); 
 
+function generateAlienShip(){
+    return {
+        hull: Math.floor(Math.random() * (6 - 3 + 1)) + 3, 
+        firepower: Math.floor(Math.random() * (4 - 2 + 1)) + 2, 
+        accuracy: Math.random() * (0.8 - 0.6) + 0.6
+    };
+    }
